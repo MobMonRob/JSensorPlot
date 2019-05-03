87b9c1fa3396e2cfcd3c6857b2b1f3688ee32103
Fabian Hinderer <uypcr@student.kit.edu>
2019-05-03 12:59:39 +0200

    copied last version into repository

---
+++ JSensorPlot/src/jsensorplot/JSensorPlot.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jsensorplot;
+
+/**
+ *
+ * @author MobMonRob
+ */
+public class JSensorPlot {
+
+    Plot plot;
+    SensorDataProcessor sensorDataProcessor;
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JSensorPlot sensorPlot = new JSensorPlot();
+        sensorPlot.init();
+    }
+
+    public JSensorPlot() {
+        plot = new Plot();
+        sensorDataProcessor = new SensorDataProcessor();
+    }
+
+    public void init() {
+        System.out.println("SensorPlot.init()");
+
+        plot.display();
+        sensorDataProcessor.init();
+        this.loop();
+    }
+
+    public void loop() {
+        DataPointCoordinatesList allDataPointCoordinates = new DataPointCoordinatesList();
+
+        while (true) {
+            allDataPointCoordinates.addDataPoint(sensorDataProcessor.getNextDataPoint());
+            plot.updateDatePointCoordinatesList(allDataPointCoordinates);
+            plot.repaint();
+        }
+    }
+}
